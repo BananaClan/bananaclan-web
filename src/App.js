06591157d9fd2426ceda63b590f4a3be95f4d2f8
ijas.j <ijas.a@marketfeed.com>
2024-12-30 23:45:10 +0530

    cart and profile drawer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage";
 // import SellerProfilePage from "./pages/SellerProfilePage";
 import ProductDetailPage from "./pages/Sneaker Marketplace Product Detail Page";
 import BagPage from "./pages/BagPage";
@@ -9,16 +9,18 @@ import OrderSuccessPage from "./pages/OrderSuccessPage";
 import { Footer } from "./components/common/Footer";
 import { CartProvider } from "./context/CartContext";
 import OrderConfirmation from "./pages/OrderConfirmation";
+import NavBar from "./components/common/NavBar";
 
 const App = () => {
   return (
     <CartProvider>
+      <NavBar></NavBar>
       <Router>
         <div className="min-h-screen flex flex-col">
           <main className="flex-grow">
             <Routes>
               <Route path="/porductdetail" element={<ProductDetailPage />} />
-          
+
               <Route path="/" element={<LandingPage />} />
               <Route path="/bag" element={<BagPage />} />
               <Route path="/shipping" element={<ShippingPage />} />
